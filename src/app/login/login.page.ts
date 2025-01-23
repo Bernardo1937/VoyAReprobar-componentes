fbf09a9c9d1d1d9957b73d4adc81fb8a79bd5a91
Bernardo1937 <20233L001143@utcv.edu.mx>
2025-01-23 09:29:23 -0600

    Crear un formulario

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 
 @Component({
@@ -9,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email : String = ''
+  password : String;
+  constructor(   private navController : NavController) {
+    this.password = ''
+   }
 
   ngOnInit() {
+  }
+  login( form : NgForm){
+    /*console.log('email: ' this.email);
+    console.log ('Password: ', this.password) */
+    console.log(form.value);
+
+    //validar formulario
+    console.log('valid: ',form.valid);
+
+    if(form.invalid){
+      console.log("Todos los campos son requeridos")
+    } else {
+      console.log("Login incorrecto")
+    }
+
+    if (this.email === ''){
+
+    }
   }
 
 }
